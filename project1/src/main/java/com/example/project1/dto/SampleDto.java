98960530c5bbf942910dc05193b626c706fcdf2f
신건호 <shingh1283@gmail.com>
2024-10-25 17:49:06 +0900

    241025

---
+++ project1/src/main/java/com/example/project1/dto/SampleDto.java
@@ -0,0 +1,16 @@
+package com.example.project1.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class SampleDto {
+    private Long id;
+    private String first;
+    private String last;
+    private LocalDateTime regDateTime;
+
+}
