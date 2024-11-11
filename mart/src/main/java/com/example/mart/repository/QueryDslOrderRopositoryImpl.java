673790750598e0428be52aa009087a4302cb54b1
신건호 <shingh1283@gmail.com>
2024-11-11 17:52:33 +0900

    241111

--- mart/src/main/java/com/example/mart/repository/QueryDslOrderRopositoryImpl.java
+++ mart/src/main/java/com/example/mart/repository/QueryDslOrderRopositoryImpl.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import com.example.mart.entity.item.Item;
 import com.example.mart.entity.item.Member;
 import com.example.mart.entity.item.Order;
+import com.example.mart.entity.item.QMember;
+import com.querydsl.jpa.JPQLQuery;
 
 public class QueryDslOrderRopositoryImpl extends QuerydslRepositorySupport implements QueryDslOrderRopository {
 
@@ -16,8 +18,14 @@ public class QueryDslOrderRopositoryImpl extends QuerydslRepositorySupport imple
 
     @Override
     public List<Member> members() {
-        return null;
+        // select *from member where name = 'user1' order by name desc
+        QMember qMember = QMember.member;
+        JPQLQuery<Member> query = from(qMember);
+        query.where(qMember.name.eq("user1")).orderBy(qMember.name.desc());
+        JPQLQuery<Member> tuple = query.select(qMember);
+        System.out.println(tuple);
 
+        return tuple.fetch();
     }
 
     @Override
